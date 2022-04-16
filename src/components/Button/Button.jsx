5237fa0f5d85852b17208289a77839f0f4bb8b6b
Riley Boyd <contact@rileyboyd.com>
2022-04-16 16:03:15 -0400

    Adding PropTypes to Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,15 @@
+import PropTypes from "prop-types";
+
 const Button = ({ onClick, children, disabled = "false" }) => (
   <button onClick={onClick} disabled={disabled}>
     {children}
   </button>
 );
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
 
 export default Button;
