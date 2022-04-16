6f10535579b4862acea77b3e47c558103e346c88
Riley Boyd <contact@rileyboyd.com>
2022-04-16 15:51:49 -0400

    Removing logo, updates to App.js and App.css. Button is now vertically centered

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
 function App() {
+  const [btnWasClicked, setBtnWasClicked] = useState(false);
+  const btnClickHandler = (e) => {
+    setBtnWasClicked(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="btn-wrapper">
+        <Button onClick={btnClickHandler}>Click Me</Button>
+      </section>
     </div>
   );
 }
